5d4eeea2ba81a83461efe6c046418bbee4b7c292
Dhruv Mehta <39973939+Dhruvmehta18@users.noreply.github.com>
2019-07-28 22:37:38 +0530

    added serviceworker

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {App} from "./components/";
 import "./styles.css";
+import {register} from './serviceWorker';
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
+register();
